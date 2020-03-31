11ecbd1e27dfaa39a16f6d010ddab40190e6e72a
webnetkz <8gg001@gmail.com>
2020-03-31 19:48:52 +0600

    new project wechat service account

---
+++ about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>О нас</title>
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,700" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-wordpress">
+          <span class="sk-inner-circle"></span>
+     </div>
+</div>
+
+<!-- Navigation section  -->
+
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+     <div class="container">
+
+     <div class="navbar-header">
+               <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+               </button>
+               <a href="tips.php" class="navbar-brand"><img src="images/miniLogo.png" alt="logo dwe"></a>
+          </div>
+          <div class="collapse navbar-collapse">
+               <ul class="nav navbar-nav navbar-right">
+               <li class="active"><a href="order.php">Оформить</a></li>
+                    <li><a href="contact.php">Контакты</a></li>
+                    <li><a href="about.php">О нас</a></li>
+                    <li><a href="services.php">Услуги</a></li>
+                    <li class="active"><a href="tips.php">Советы</a></li>
+                    <hr>
+                    <li class="active"><a href="https://home.courierexe.ru/290/tracking/">Трекинг</a></li>
+                    <li class="active"><a href="https://home.courierexe.ru/290/auth/login">Кабинет</a></li>
+               </ul>
+          </div>
+
+  </div>
+</div>
+
+
+
+
+<!-- SCRIPTS -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
---
+++ tips.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Советы DWExpress</title>
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,700" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-wordpress">
+          <span class="sk-inner-circle"></span>
+     </div>
+</div>
+
+<!-- Navigation section  -->
+
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+     <div class="container">
+
+          <div class="navbar-header">
+               <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+               </button>
+               <a href="tips.php" class="navbar-brand"><img src="images/miniLogo.png" alt="logo dwe"></a>
+          </div>
+          <div class="collapse navbar-collapse">
+               <ul class="nav navbar-nav navbar-right">
+               <li class="active"><a href="order.php">Оформить</a></li>
+                    <li><a href="contact.php">Контакты</a></li>
+                    <li><a href="about.php">О нас</a></li>
+                    <li><a href="services.php">Услуги</a></li>
+                    <li class="active"><a href="tips.php">Советы</a></li>
+                    <hr>
+                    <li class="active"><a href="https://home.courierexe.ru/290/tracking/">Трекинг</a></li>
+                    <li class="active"><a href="https://home.courierexe.ru/290/auth/login">Кабинет</a></li>
+               </ul>
+          </div>
+
+  </div>
+</div>
+
+<!-- Home Section -->
+
+<section id="home">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-12 col-sm-12">
+                    <hr>
+                    <h2>Советы по бесплатному ускорению доставки</h2>
+    <p>Из-за множества не зависящих от нас факторов – ошибки в оформлении заказов, нерегулярная обратная связь со стороны клиентов и др могут вызвать задержки при обработке посылок. </p>
+    <p>Мы проанализировали самые распространённые ошибки и подготовили для вас несколько полезных советов, которые реально помогут вам ускорить процесс обработки и отправки ваших посылок.</p>
+    <br>
+    <p><b>Самостоятельное оформление.</b> Всегда оформляйте свои посылки самостоятельно а не через продавца. Так как при возникновении каких-либо вопросов или проблем,связанных с посылкой, вы сможете напрямую решать их со службой клиентской поддержки DWExpress – это значительно сократит время и ускорит процесс обработки отправления. Также самостоятельное оформление – это доступ в личный кабинет DWExpress, где вам будет доступна полная информация о всех ваших отправлениях.</p>
+    <p><b>Оформление.</b> Если у вас только одна посылка, то не ждите её поступления  на наш склад, оформляйте  заказ сразу как только узнали трек-номер: при поступлении посылок на склад, заранее оформленные посылки сразу же поступают в обработку а не  на полку временного хранения.  Внимательно заполняйте все контактные данные получателя. Несмотря на очевидность этого совета, неточности в адресах и номерах телефона - самая распространённая ошибка, которую совершают наши клиенты. Любое несоответствие в контактных данных ведёт к задержке и к лишним финансовым тратам за внесение изменений в заказ.</p>
+    <p><b>Объединение посылок.</b> Если в ваш заказ входит несколько посылок от разных поставщиков, оформляйте его только тогда, когда будете знать трек-номера всех посылок. Следуя этим рекомендациям вы сэкономите  не только время, но и деньги. Также  стоит обратить внимание на то,что  объединить посылки можно только в том случае, если они все поступили на один склад.</p>
+    <p><b>Разделение посылки.</b> Очень часто возникают ситуации, когда продавец отправляет вам посылку, которую  нужно разделить для разных получателей. В этом случае вы оформляете в системе посылку, но при оформлении в примечаниях  указываете какую именно вещь отправить по этому оформлению  и каким вещам присвоить трек-номера. В такой ситуации нужно обязательно заказывать фотоотчёт, и при разделении посылки вам в личном кабинете отправят фото товаров с трек-номерами. Оформляя посылку таким образом, вы реально ускоряете процесс обработки и доставки.</p>
+    <p><b>Оплата.</b> При совершении оплаты обязательно нужно указывать трек-номер оплачиваемой посылки в сообщении к платежу. При оплате заказа через - WeChat, Alipay или через банковскую карту, есть возможность сразу указать за какую посылку вы переводите деньги, тогда статус оплаты в системе изменится автоматически. Оплачивая посылку таким образом,вы реально ускоряете  процесс обработки и доставки.</p>
+    <p><b>Отслеживание.</b> Чаще проверяйте статус посылки в личном кабинете в приложении или на сайте. Иногда курьер по разным причинам не может дозвониться до получателя и договориться о времени доставки, хотя посылка уже находится в вашем городе.Чем раньше вы узнаете о проблеме – тем быстрее мы её решим. </p>
+    <p>Надеемся, что эти советы помогут вам сделать пользование нашим сервисом ещё более удобным и полезным.</p>
+
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- Portfolio Section -->
+
+
+<!-- 
+
+<footer>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-3 col-sm-3">
+                    <i class="fa fa-magnet"></i>
+               </div>
+
+               <div class="col-md-4 col-sm-4">
+                    <p>124 Market Street, Suite 3570 San Francisco, CA 3042 United States</p>
+               </div>
+
+               <div class="col-md-offset-1 col-md-4 col-sm-offset-1 col-sm-3">
+                    <p><a href="mailto:youremail@gmail.com">hello@yourstudio.co</a></p>
+                    <p>(+01) 2048937 / 02 203403</p>
+               </div>
+
+               <div class="clearfix col-md-12 col-sm-12">
+                    <hr>
+               </div>
+
+               <div class="col-md-6 col-sm-6">
+                    <div class="footer-copyright">
+                         <p>© 2016 Magnet Studio | All Rights Reserved.</p>
+                    </div>
+               </div>
+
+               <div class="col-md-6 col-sm-6">
+                    <ul class="social-icon">
+                         <li><a href="#" class="fa fa-facebook"></a></li>
+                         <li><a href="#" class="fa fa-twitter"></a></li>
+                         <li><a href="#" class="fa fa-linkedin"></a></li>
+                    </ul>
+               </div>
+               
+          </div>
+     </div>
+</footer>
+
+
+SCRIPTS -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
